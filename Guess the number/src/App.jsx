ce80f01faa1d8623a10d64fdd3539641845c50ac
alexxtomas <atomasllimos@gmail.com>
2023-01-04 17:29:17 +0100

    Guess the number: Add title

---
+++ Guess the number/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="bg-zinc-900 min-h-screen">
+      <header className="flex justify-center items-center h-20 border-b-2 border-gray-400">
+        <h1 className="text-3xl font-medium text-gray-200  ">
+          Guess the number
+        </h1>
+      </header>
+    </div>
+  )
+}
