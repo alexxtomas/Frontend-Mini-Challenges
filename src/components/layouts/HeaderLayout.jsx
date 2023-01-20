792b2467674dc121db12f1052123453ad5dd1b21
alexxtomas <atomasllimos@gmail.com>
2023-01-20 11:26:51 +0100

    Add Link to HeaderLayout

--- src/components/layouts/HeaderLayout.jsx
+++ src/components/layouts/HeaderLayout.jsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default function HeaderLayout({ children }) {
   return (
     <header className='flex justify-center items-center h-20 border-b-2 border-gray-400 min-w-full'>
-      <h1 className='text-xl font-bold text-gray-200'>{children}</h1>
+      <Link href={'/'}>
+        <h1 className='text-xl font-bold text-gray-200'>{children}</h1>
+      </Link>
     </header>
   )
 }
