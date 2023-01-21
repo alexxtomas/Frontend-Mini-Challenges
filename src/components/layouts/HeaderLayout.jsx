e20e10ebb8af57b825e3a56f56588c58c18c6029
alexxtomas <atomasllimos@gmail.com>
2023-01-21 23:08:05 +0100

    Create layouts

--- src/components/layouts/HeaderLayout.jsx
+++ src/components/layouts/HeaderLayout.jsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function HeaderLayout({ children }) {
   return (
-    <header className='flex justify-center items-center h-20 border-b-2 border-gray-400 min-w-full'>
+    <header className='flex justify-center items-center px-4   h-20 border-b-2 border-gray-400 min-w-full'>
       <Link href={'/'}>
-        <h1 className='text-xl font-bold text-gray-200'>{children}</h1>
+        <h1 className='text-2xl font-bold text-gray-200 text-center'>{children}</h1>
       </Link>
     </header>
   )
--- src/components/layouts/PageLayout.jsx
+++ src/components/layouts/PageLayout.jsx
@@ -1,3 +1,3 @@
 export default function PageLayout({ children }) {
-  return <div className='bg-zinc-900 min-h-screen'>{children}</div>
+  return <div className='bg-zinc-900 min-h-screen flex flex-col gap-10'>{children}</div>
 }
