96cd082f3789c12909d40f881a24e74415e3a17b
alexxtomas <atomasllimos@gmail.com>
2023-01-21 23:16:18 +0100

    Add challenge cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,8 @@
 import Head from '@/components/Head'
 import HeaderLayout from '@/components/layouts/HeaderLayout'
 import MainLayout from '@/components/layouts/MainLayout'
+import { CHALLENGES } from '@/constants'
 import Link from 'next/link'
-const CHALLENGES = [
-  {
-    name: 'Counter',
-    path: './challenge/counter'
-  },
-  {
-    name: 'Guess the number',
-    path: './challenge/guess-the-number'
-  },
-  {
-    name: 'Telephone Formatter',
-    path: './challenge/telephone-formatter'
-  }
-]
 
 export default function Home() {
   return (
@@ -23,10 +10,13 @@ export default function Home() {
       <Head />
       <HeaderLayout>Frontend Mini Challenges - Solutions by alexxtomas</HeaderLayout>
       <MainLayout>
-        <ul>
+        <ul className='grid grid-cols-2 gap-10 my-5 '>
           {CHALLENGES.map(({ name, path }) => {
             return (
-              <li key={name}>
+              <li
+                key={name}
+                className='text-gray-200 flex items-center justify-center flex-col border py-8 px-2 rounded text-center cursor-pointer transition-colors h-14 w-30 hover:bg-green-400   hover:text-gray-800 hover:border-green-400 scale-105'
+              >
                 <Link href={path}>{name}</Link>
               </li>
             )
