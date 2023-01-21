fee5db5bd7dcce139e7abb91e0bc05fa729d3300
alexxtomas <atomasllimos@gmail.com>
2023-01-21 23:07:42 +0100

    Add contants file

---
+++ src/constants.js
@@ -0,0 +1,128 @@
+const PREFIX = './challenge'
+
+export const CHALLENGES = [
+  {
+    name: 'Counter',
+    path: `${PREFIX}/counter`
+  },
+  {
+    name: 'Guess the number',
+    path: `${PREFIX}/guess-the-number`
+  },
+  {
+    name: 'Telephone formatter',
+    path: `${PREFIX}/telephone-formatter`
+  },
+  {
+    name: 'Tic Tac Toe',
+    path: `${PREFIX}/tic-tac-toe`
+  },
+  {
+    name: 'Light & Dark mode',
+    path: `${PREFIX}/light-and-dark-mode`
+  },
+  {
+    name: 'Toast Popup',
+    path: `${PREFIX}/toast-popup`
+  },
+  {
+    name: 'CSS shapes',
+    path: `${PREFIX}/css-shapes`
+  },
+  {
+    name: 'Password strength',
+    path: `${PREFIX}/password-strength`
+  },
+  {
+    name: 'Star rating',
+    path: `${PREFIX}/star-rating`
+  },
+  {
+    name: 'Pixel Art',
+    path: `${PREFIX}/pixel-art`
+  },
+  {
+    name: 'Color spotter',
+    path: `${PREFIX}/color-spotter`
+  },
+  {
+    name: 'Todo list',
+    path: `${PREFIX}/todo-list`
+  },
+  {
+    name: 'Transfer list',
+    path: `${PREFIX}/transfer-list`
+  },
+  {
+    name: 'Chess board',
+    path: `${PREFIX}/chess-board`
+  },
+  {
+    name: 'Chips Input',
+    path: `${PREFIX}/chips-input`
+  },
+  {
+    name: 'Password generator',
+    path: `${PREFIX}/password-generator`
+  },
+  {
+    name: 'Analog clock',
+    path: `${PREFIX}/analog-clock`
+  },
+  {
+    name: 'Basic calculator',
+    path: `${PREFIX}/basic-calculator`
+  },
+  {
+    name: 'Math pair',
+    path: `${PREFIX}/match-pair`
+  },
+  {
+    name: 'Typing speed test',
+    path: `${PREFIX}/typing-speed-text`
+  },
+  {
+    name: 'Countdown timer',
+    path: `${PREFIX}/countdown-timer`
+  },
+  {
+    name: 'OTP input',
+    path: `${PREFIX}/otp-input`
+  },
+  {
+    name: 'Area selector',
+    path: `${PREFIX}/area-selector`
+  },
+  {
+    name: 'Carousel',
+    path: `${PREFIX}/carousel`
+  },
+  {
+    name: 'Calendar',
+    path: `${PREFIX}/calendar`
+  },
+  {
+    name: 'Paginator',
+    path: `${PREFIX}/paginator`
+  },
+  {
+    name: 'Connet four',
+    path: `${PREFIX}/connet-four`
+  },
+  {
+    name: 'Comment box',
+    path: `${PREFIX}/comment-box`
+  },
+  {
+    name: 'JSON creator',
+    path: `${PREFIX}/json-creator`
+  },
+  {
+    name: 'Array methods',
+    path: `${PREFIX}/array-methods`
+  },
+  {
+    name: 'Progress bar',
+    path: `${PREFIX}/progress-bar`
+  }
+]
