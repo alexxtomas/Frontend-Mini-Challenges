6815aeaaf55e6f4c3cb4146ddbc67d22ecf9cef3
alexxtomas <atomasllimos@gmail.com>
2023-01-22 23:03:30 +0100

    Use Turn compnent

--- src/components/tic-tac-toe/GameTurns.jsx
+++ src/components/tic-tac-toe/GameTurns.jsx
@@ -1,22 +1,12 @@
 import { OPTION } from '@/constants/tic-tac-toe/index'
 import Cell from './Cell'
+import Turn from './Turn'
 export default function GameTurns({ turn }) {
   return (
-    <section className='flex w-[300px] h-[300px] justify-evenly mx-auto gap-10 mt-8 text-4xl '>
-      <Cell
-        styles={` ${
-          turn === OPTION.X && 'bg-green-500'
-        } rounded-xl w-[100px] h-[100px] flex items-center justify-center`}
-      >
-        {OPTION.X}
-      </Cell>
-      <Cell
-        styles={` ${
-          turn === OPTION.O && 'bg-green-500'
-        } rounded-xl w-[100px] h-[100px] flex items-center justify-center`}
-      >
-        {OPTION.O}
-      </Cell>
+    <section className='flex w-[300px] h-[300px] justify-evenly mx-auto gap-10 mt-8 bg-z '>
+      {Object.values(OPTION).map((option) => {
+        return <Turn key={option} highlight={turn === option} option={option} />
+      })}
     </section>
   )
 }
