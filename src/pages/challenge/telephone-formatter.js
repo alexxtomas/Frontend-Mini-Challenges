8df0c6d22874894f4114c5878e64a0532f5c5ac4
alexxtomas <atomasllimos@gmail.com>
2023-01-24 00:44:01 +0100

    Use new logic

--- src/pages/challenge/telephone-formatter.js
+++ src/pages/challenge/telephone-formatter.js
@@ -1,66 +1,15 @@
 import Head from '@/components/Head'
 import HeaderLayout from '@/components/layouts/HeaderLayout'
 import MainLayout from '@/components/layouts/MainLayout'
+import { formatNumber } from '@/logic/telephone-formatter/formatPhone'
 import { useState } from 'react'
 
 export default function TelephoneFormatter() {
   const [mobileNumber, setMobileNumber] = useState('')
-  const [isFormatted, setIsFormatted] = useState(false)
-  const [prefix, setPrefix] = useState('')
 
   const handleChange = ({ target: { value } }) => {
-    const prefixRegex = /\+\([0-9][0-9][0-9]\)-/
-    const onlyDigitsRegex = /^\d+$/
-    const { length } = value
-    let newPrefix = undefined
-
-    if (value === prefix) {
-      console.log('a')
-      setPrefix('')
-    }
-
-    if (length === 16) {
-      return
-    }
-
-    if (length > 16) {
-      const newValue = value.slice(0, 16)
-      value = newValue
-    }
-
-    if (!onlyDigitsRegex.test(value)) {
-      const arrayFromValue = value.split('')
-      const fromattedArray = arrayFromValue.filter((character) => onlyDigitsRegex.test(character))
-
-      value = fromattedArray.join('')
-    }
-
-    if (length <= 0 && isFormatted) {
-      setIsFormatted(false)
-    }
-    if (length > 4) {
-      if (!isFormatted) {
-        let prefixArray = []
-        for (let i = 0; i <= 6; i++) {
-          prefixArray.push(value[i])
-        }
-        const currentPrefix = prefixArray.join('')
-        newPrefix = !prefixRegex.test(currentPrefix)
-          ? `+(${value[0]}${value[1]}${value[2]})-`
-          : currentPrefix
-
-        setIsFormatted(true)
-      }
-      value = value.slice(3)
-    }
-
-    let prefixToUse = prefix
-
-    if (newPrefix) {
-      setPrefix(newPrefix)
-      prefixToUse = newPrefix
-    }
-    setMobileNumber(`${prefixToUse}${value}`)
+    const newValue = formatNumber(value)
+    setMobileNumber(newValue)
   }
 
   return (
