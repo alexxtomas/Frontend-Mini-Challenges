00b72df16c2724e3bc4be47b9d28daeac7f63c60
alexxtomas <atomasllimos@gmail.com>
2023-01-23 19:18:56 +0100

    Change height style

--- src/components/tic-tac-toe/GameTurns.jsx
+++ src/components/tic-tac-toe/GameTurns.jsx
@@ -3,7 +3,7 @@ import Cell from './Cell'
 import Turn from './Turn'
 export default function GameTurns({ turn }) {
   return (
-    <section className='flex w-[300px] h-[300px] justify-evenly mx-auto gap-10 mt-8 bg-z '>
+    <section className='flex w-[300px] h-32 justify-evenly mx-auto gap-10 mt-8 bg-z '>
       {Object.values(OPTION).map((option) => {
         return <Turn key={option} highlight={turn === option} option={option} />
       })}
