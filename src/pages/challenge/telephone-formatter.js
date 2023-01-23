ace71d60ef57a9ac62ab57e62ed96a2c90652c0d
alexxtomas <atomasllimos@gmail.com>
2023-01-23 19:20:13 +0100

    Add input with some styles

--- src/pages/challenge/telephone-formatter.js
+++ src/pages/challenge/telephone-formatter.js
@@ -1,15 +1,36 @@
 import Head from '@/components/Head'
 import HeaderLayout from '@/components/layouts/HeaderLayout'
 import MainLayout from '@/components/layouts/MainLayout'
+import { useState } from 'react'
 
 export default function TelephoneFormatter() {
+  const [mobileNumber, setMobileNumber] = useState('')
+
+  const handleChange = ({ target: { value } }) => {
+    console.log(value)
+    setMobileNumber(value)
+  }
   return (
     <>
       <Head title='Guess the number' />
       <HeaderLayout>Telephone formatter</HeaderLayout>
       <MainLayout>
-        <input />
+        <label htmlFor='mobileNumber' className='hidden'>
+          Enter a mobile number to format
+        </label>
+        <input
+          id='mobileNumber'
+          className=' rounded pl-4 h-8 w-64 text-black text-xl'
+          placeholder='Mobile number'
+          onChange={handleChange}
+          value={mobileNumber}
+        />
       </MainLayout>
     </>
+    /// Three first numbers shoud be into ()
+    // max length of 9
+    //  no letters
+    // prefix - numbers
+    // +(123) 679911255
   )
 }
