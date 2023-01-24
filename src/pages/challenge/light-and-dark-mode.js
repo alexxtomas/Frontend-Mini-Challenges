2de132a73bfe118230f6d0592beeae5ff33e688f
alexxtomas <atomasllimos@gmail.com>
2023-01-24 11:27:24 +0100

    Complete light-and-dark challenge

--- src/pages/challenge/light-and-dark-mode.js
+++ src/pages/challenge/light-and-dark-mode.js
@@ -1,7 +1,25 @@
 import Head from '@/components/Head'
 import HeaderLayout from '@/components/layouts/HeaderLayout'
 import MainLayout from '@/components/layouts/MainLayout'
+import { useEffect } from 'react'
 export default function LightAndDarkMode() {
+  const userTheme = localStorage.getItem('theme')
+  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  const handleChangeDarkMode = () => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark')
+      window.localStorage.setItem('theme', 'light')
+      return
+    }
+    document.documentElement.classList.add('dark')
+    window.localStorage.setItem('theme', 'dark')
+  }
+  const handleChangeContainedDarkMode = () => {
+    const container = document.querySelector('#container-dark-mode')
+    container.toggleAttribute('data-dark')
+  }
+
   return (
     <>
       <Head title='Light & Dark Mode' />
@@ -10,7 +28,7 @@ export default function LightAndDarkMode() {
         <section className='p-5'>
           <label className='text-xl'>
             Dark mode
-            <input type='checkbox' />
+            <input type='checkbox' onChange={handleChangeDarkMode} />
           </label>
           <div className='text-start min-w-full '>
             <p className='text-4xl'>Te entire page color theme is changed</p>
@@ -19,13 +37,30 @@ export default function LightAndDarkMode() {
         <section className='mt-10 p-5'>
           <label className='block'>
             Contained dark mode
-            <input type='checkbox' />
+            <input type='checkbox' onChange={handleChangeContainedDarkMode} />
           </label>
-          <div className='min-w-full border text-center h-52 flex items-center justify-center'>
+          <div
+            id='container-dark-mode'
+            className='min-w-full bg-zinc-100 border  text-black border-black  text-center h-52 flex items-center justify-center dark:text-gray-200 dark:bg-zinc-900 dark:border-white '
+          >
             <p className='text-4xl'>The container color shcema is only changed</p>
           </div>
         </section>
       </MainLayout>
+      <style jsx>
+        {`
+          #container-dark-mode {
+            background-color: #f4f4f5;
+            color: #000;
+            border-color: #000;
+          }
+          #container-dark-mode[data-dark] {
+            background-color: #18181b;
+            color: #e5e7eb;
+            border-color: #fff;
+          }
+        `}
+      </style>
     </>
   )
 }
