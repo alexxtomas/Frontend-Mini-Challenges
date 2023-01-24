2c4d190d8ed3eb497121c23834efd9b1b426fa8e
alexxtomas <atomasllimos@gmail.com>
2023-01-24 19:12:36 +0100

    Personalize toast through the propos

--- src/components/toast-popup/Toast.jsx
+++ src/components/toast-popup/Toast.jsx
@@ -1,8 +1,12 @@
+import { TOAST_TYPES } from '@/constants/toast-popup'
 export default function Toast({ type }) {
+  const selectedToast = TOAST_TYPES.find(({ name }) => name === type)
   return (
-    <div className='flex justify-between items-center px-3 py-2 text-black max-w-[300px] bg-green-400 rounded '>
+    <div
+      className={`flex justify-between  items-center px-3 py-2 text-black max-w-[300px] ${selectedToast.color} rounded`}
+    >
       <div>
-        <span className='font-bold'>✓ </span>This is a toast message!
+        <span className='font-bold pr-2'>{selectedToast.symbol}</span>This is a toast message!
       </div>
       <div className='font-bold'>X</div>
     </div>
