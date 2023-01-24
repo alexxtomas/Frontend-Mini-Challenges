8b3e5919c46e1405140024c871f543bfb32093a3
alexxtomas <atomasllimos@gmail.com>
2023-01-24 19:28:59 +0100

    Change select width

--- src/components/toast-popup/Select.jsx
+++ src/components/toast-popup/Select.jsx
@@ -3,7 +3,7 @@ export default function Select({ options, name, handleChange }) {
     <select
       name={name}
       onChange={handleChange}
-      className='w-20 border border-black h-8 rounded text-black text-center'
+      className='w-24 border border-black h-8 rounded text-black text-center'
     >
       {options.map((option, index) => {
         return (
