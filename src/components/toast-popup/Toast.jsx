be683d1e484dde0204cb492bacbed458d92e9af3
alexxtomas <atomasllimos@gmail.com>
2023-01-24 19:18:17 +0100

    Add props to close toast and add a custom message

--- src/components/toast-popup/Toast.jsx
+++ src/components/toast-popup/Toast.jsx
@@ -1,14 +1,17 @@
 import { TOAST_TYPES } from '@/constants/toast-popup'
-export default function Toast({ type }) {
+export default function Toast({ type, setShowToast, text }) {
   const selectedToast = TOAST_TYPES.find(({ name }) => name === type)
   return (
     <div
       className={`flex justify-between  items-center px-3 py-2 text-black max-w-[300px] ${selectedToast.color} rounded`}
     >
       <div>
-        <span className='font-bold pr-2'>{selectedToast.symbol}</span>This is a toast message!
+        <span className='font-bold pr-2'>{selectedToast.symbol}</span>
+        {text}
       </div>
-      <div className='font-bold'>X</div>
+      <div onClick={() => setShowToast(false)} className='font-bold cursor-pointer'>
+        X
+      </div>
     </div>
   )
 }
