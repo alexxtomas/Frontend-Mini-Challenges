a5f5fc3403b331fe121c2c05cb3fa5d95b8d63f2
alexxtomas <atomasllimos@gmail.com>
2023-01-24 18:59:02 +0100

    Create Select component

---
+++ src/components/toast-popup/Select.jsx
@@ -0,0 +1,17 @@
+export default function Select({ options, name, handleChange }) {
+  return (
+    <select
+      name={name}
+      onChange={handleChange}
+      className='w-20 border border-black h-8 rounded text-black text-center'
+    >
+      {options.map((option, index) => {
+        return (
+          <option key={`${option}-${index}`} value={option}>
+            {option}
+          </option>
+        )
+      })}
+    </select>
+  )
+}
