48a987c5a7de984a4fba0484a6f2ccd1ccfd7bdd
alexxtomas <atomasllimos@gmail.com>
2023-01-25 15:35:47 +0100

    Add new props and format some props

--- src/components/toast-popup/Toast.jsx
+++ src/components/toast-popup/Toast.jsx
@@ -1,16 +1,23 @@
 import { TOAST_TYPES } from '@/constants/toast-popup'
-export default function Toast({ type, setShowToast, text }) {
+export default function Toast({ type, setShowToast, text, duration, side, position }) {
   const selectedToast = TOAST_TYPES.find(({ name }) => name === type)
+  const lowerCaseSide = side.toLowerCase()
+  const lowerCasePosition = position.toLowerCase()
+
+  setTimeout(() => {
+    setShowToast(false)
+  }, duration * 1000)
+
   return (
     <div
-      className={`flex justify-between  items-center px-3 py-2 text-black max-w-[300px] ${selectedToast.color} rounded`}
+      className={`flex justify-between absolute ${lowerCasePosition}-4 ${lowerCaseSide}-4  items-center px-3  text-black max-w-[300px] ${selectedToast.color} rounded`}
     >
       <div>
         <span className='font-bold pr-2'>{selectedToast.symbol}</span>
         {text}
       </div>
-      <div onClick={() => setShowToast(false)} className='font-bold cursor-pointer'>
-        X
+      <div onClick={() => setShowToast(false)} className=' cursor-pointer pl-5 pb-[2px] text-3xl'>
+        ×
       </div>
     </div>
   )
